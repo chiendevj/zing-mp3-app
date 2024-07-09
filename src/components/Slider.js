e647124a2f50509d2bcdebd4b105cc57b55c1c14
ttchien <chientt.00.student@fit.tdc.edu.vn>
2024-07-09 10:35:50 +0700

    doing player

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
-
+import { useSelector, useDispatch } from 'react-redux';
+import * as actions from '../store/actions'
 function Slider() {
     const { banner } = useSelector(state => state.app);
+    const dispatch = useDispatch()
+
     const [cycleIndex, setCycleIndex] = useState(0);
     const itemsToShow = 3;
     const intervalTime = 3000; // Thời gian chuyển đổi giữa các chu kỳ (ms)
@@ -23,12 +25,20 @@ function Slider() {
         return visibleItems;
     };
 
+    const handleClickBanner = (item) => {
+        if (item?.type === 4) {
+            dispatch(actions.setCurSongId('Z78EU9W8'))
+        }
+        
+    }
+
     return (
         <div className='flex overflow-hidden w-full px-14 pt-8 pb-4'>
-            {getVisibleItems().map((item, index) => (
+            {getVisibleItems()?.map((item, index) => (
                 <img
                     key={item?.encodeId}
                     src={item?.banner}
+                    onClick={() => handleClickBanner(item)}
                     className='flex-1 object-contain transition-transform duration-1000 rounded-lg w-1/3'
                     style={{
                         transform: `translateX(${index * 5}%)`
--- src/containers/public/Public.js
+++ src/containers/public/Public.js
@@ -3,20 +3,19 @@ import { Outlet } from 'react-router-dom'
 import { Player, SidebarLeft, SidebarRight } from '../../components'
 function Public() {
   return (
-    <div className='w-full min-h-screen flex flex-col'>
-      <div className='h-full flex flex-auto'>
-        <div className='w-[240px] flex-none bg-[#cce0e0]'>
+    <div className='w-full min-h-screen flex flex-col bg-main-300'>
+      <div className='h-full flex flex-auto w-full'>
+        <div className='w-[240px] flex-none min-h-screen'>
           <SidebarLeft />
         </div>
-        <div className='flex-auto bg-bgPrimary'>
+        <div className='flex-auto'>
           <Outlet />
         </div>
-        <div className='w-[330px] flex-none bg-red-300'>
+        <div className='w-[330px] hidden 1200:flex flex-none bg-red-300 animate-slide-left'>
           <SidebarRight />
-
         </div>
       </div>
-      <div className='flex-none h-[90px] bottom-0 left-0 bg-[#c0d8d8] w-full'>
+      <div className='flex-none h-[90px] fixed left-0 bottom-0 w-full'>
         <Player/>
       </div>
     </div>
