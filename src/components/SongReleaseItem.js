e3fc5a66eeb4182fb5246a8c6baf05c6ffa5f360
ttchien <chientt.00.student@fit.tdc.edu.vn>
2024-07-21 14:33:06 +0700

    doing home chart

--- src/components/SongReleaseItem.js
+++ src/components/SongReleaseItem.js
@@ -2,12 +2,14 @@ import React, { memo } from 'react';
 import icons from '../untils/icons';
 import moment from 'moment';
 import 'moment/locale/vi';
+import clsx from 'clsx';
 
 import * as actions from '../store/actions';
 import { useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
-function SongReleaseItem({ item, order, percent }) {
+function SongReleaseItem({ item, order, percent, color }) {
+
     moment.locale('vi')
     const dispatch = useDispatch();
     const handleCLickSong = (sid) => {
@@ -15,65 +17,79 @@ function SongReleaseItem({ item, order, percent }) {
         dispatch(actions.playAlbum(true));
         dispatch(actions.play(true));
     }
+
     const textOrders = [
         '#4a90e2', '#27bd9c', '#e35050'
     ]
+
+    const backgroundColor = clsx({
+        [`bg-[${color}]`]: color  && !order && percent,
+        'bg-[rgba(239,216,255,0.4)] hover:bg-[rgba(239,216,255,0.5)]': !color && order && percent,
+        'hover:bg-main-200 bg-main-300 hover:border-main-200': !order && !color && !percent,
+    });
+
+    console.log(backgroundColor);
     return (
-        <div className={` ${order ? 'bg-[rgba(239,216,255,0.7)]' : 'hover:bg-main-200 hover:border-main-200'} group flex gap-2 p-[10px] rounded cursor-pointer items-center justify-between w-full`}>
+        <div className={`group flex gap-2 p-[10px] rounded cursor-pointer items-center justify-between w-full ${backgroundColor}`}>
             <div className='flex gap-2 items-center'>
-            {order &&
-                <span
-                    style={{
-                        '-webkit-text-stroke': `1px ${textOrders[order - 1]}`
-                    }}
-                    className='text-4xl mx-2 text-transparent'>
-                    {order}
-                </span>
-            }
-            <div className='relative rounded overflow-hidden w-[60px] h-[60px]'>
-                <img
-                    src={item?.thumbnailM}
-                    alt={item?.title}
-                    title={item?.title}
-                    className='w-full h-auto transform transition-transform duration-500 group-hover:scale-110'
-                />
-                <div className='absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-30 flex items-center justify-center transition-opacity duration-300'>
-                    <button
-                        onClick={() => { handleCLickSong(item.encodeId) }}
-                        className='absolute z-10 text-white opacity-0 group-hover:opacity-100 p-1 transition-opacity duration-200'
-                    >
-                        <icons.MdPlayArrow size={25} />
-                    </button>
-                </div>
-            </div>
-            <div className='flex flex-col mt-2'>
-                <span
-                    onClick={() => { handleCLickSong(item.encodeId) }}
-                    className={`${order ? 'text-[#a995b7]' : 'text-main-600'} text-sm font-medium`}>
-                    {item?.title.length >= 30 ? `${item?.title.trim().slice(0, 30)}...` : item?.title}
-                </span>
-                <div className='text-xs text-main-700 font-normal'>
-                    {item?.artists &&
-                        item.artists.map((artist, index) => (
-                            <NavLink
-                                key={artist.id}
-                                to={`/${artist.alias}`}
-                                className="cursor-pointer hover:text-main-500 hover:underline"
-                            >
-                                {artist.name}{artist.spotlight && '★'}
-                                {index !== item.artists.length - 1 && ', '}
-                            </NavLink>
-                        ))}
-                </div>
-                {!percent &&
-                    <div className='text-xs text-main-700'>
-                        {moment.unix(item.releaseDate).fromNow()}
-                    </div>
+                {order &&
+                    <span
+                        style={{
+                            '-webkit-text-stroke': `2px ${textOrders[order - 1]}`
+                        }}
+                        className='text-[40px] mx-2 font-bold text-transparent'>
+                        {order}
+                    </span>
                 }
-            </div>
+                <div className='relative rounded overflow-hidden w-[60px] h-[60px]'>
+                    <img
+                        src={item?.thumbnailM}
+                        alt={item?.title}
+                        title={item?.title}
+                        className='w-full h-auto transform transition-transform duration-500 group-hover:scale-110'
+                    />
+                    <div className='absolute inset-0 bg-black bg-opacity-0 group-hover:bg-opacity-30 flex items-center justify-center transition-opacity duration-300'>
+                        <button
+                            onClick={() => { handleCLickSong(item.encodeId) }}
+                            className='absolute z-10 text-white opacity-0 group-hover:opacity-100 p-1 transition-opacity duration-200'
+                        >
+                            <icons.MdPlayArrow size={25} />
+                        </button>
+                    </div>
+                </div>
+                <div className='flex flex-col mt-2 leading-none'>
+                    <span
+                        onClick={() => { handleCLickSong(item.encodeId) }}
+                        className={`${order ? 'text-[#ffffff80]' : 'text-main-600'} text-sm font-medium`}>
+                        {item?.title?.length >= 30 ? `${item?.title.trim().slice(0, 30)}...` : item?.title}
+                    </span>
+                    <div className={`text-xs ${order ? 'text-[#ffffff80]' : 'text-main-700'} font-normal`}>
+                        {item?.artists &&
+                            item.artists.map((artist, index) => (
+                                <NavLink
+                                    key={artist.id}
+                                    to={`/${artist.alias}`}
+                                    className="cursor-pointer hover:text-main-500 hover:underline"
+                                >
+                                    {artist.name}{artist.spotlight && '★'}
+                                    {index !== item.artists.length - 1 && ', '}
+                                </NavLink>
+                            ))}
+                    </div>
+                    {!percent
+                        ?
+                        <div className='text-xs text-main-700'>
+                            {moment.unix(item?.releaseDate).fromNow()}
+                        </div>
+                        : ''
+                    }
+                </div>
             </div>
             {percent &&
-                <span className={`text-2xl font-bold text-[${textOrders[order-1]}]`}>
+                <span
+                    style={{ color: textOrders[order - 1] }}
+                    className={`${order ? 'text-2xl' : 'text-lg text-main-500'} font-bold`}
+                >
                     {`${percent}%`}
                 </span>
             }
